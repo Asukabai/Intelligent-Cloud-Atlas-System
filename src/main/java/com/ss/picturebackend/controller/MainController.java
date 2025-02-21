fe83869bf0f6172e16471cb8cb6e41e62dfd7d66
Asukabai <3293775641@qq.com>
2025-02-21 15:22:37 +0800

    初始化后端模板代码（包含通用的请求，响应，异常类）

---
+++ src/main/java/com/ss/picturebackend/controller/MainController.java
@@ -0,0 +1,21 @@
+package com.ss.picturebackend.controller;
+
+
+import com.ss.picturebackend.common.BaseResponse;
+import com.ss.picturebackend.common.ResultUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class MainController {
+
+    /**
+     * 健康检查
+     */
+    @GetMapping("/health")
+    public BaseResponse<String> health() {
+        return ResultUtils.success("ok");
+    }
+}
